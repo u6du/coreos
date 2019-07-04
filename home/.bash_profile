63e47fd453c29b7c9392cc8769043607c9471a44
- <->
2019-07-04 11:35:14 +0800

    init

--- home/.bash_profile
+++ home/.bash_profile
@@ -1,3 +1,3 @@
 alias log="journalctl -f -u"
-
+alias toolbox="/usr/bin/toolbox zsh"
 export PATH=~/.bin:$PATH
--- toolbox/init.sh
+++ toolbox/init.sh
@@ -1,2 +1,12 @@
-dnf install -y python3 tmux neovim git zsh curl wget util-linux-user which
+dnf install -y python3 tmux neovim git zsh curl wget util-linux-user which gcc python3-devel
+
+alternatives --install /usr/bin/python python /usr/bin/python3 1
+
+curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python
+
+
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+vipath=$(which vi)
+rm $vipath
+ln -s `which nvim` $vipath 
